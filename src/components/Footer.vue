e8bd123db0d4003595f98f855892a59d093fea27
Jovany Vergara <diego.jobany11@gmail.com>
2023-10-24 23:52:07 -0600

    [main] create new component

---
+++ src/components/Footer.vue
@@ -0,0 +1,24 @@
+<template>
+  <footer class="footer-euripcar d-flex justify-content-center">
+    <img src="/src/assets/img/autoclick.jpeg" class="d-inline-block align-text-top m-3" style="width: 5%;">
+    <div class="line-vertical"></div>
+    <div class="m-3">
+      <h6>Atención al cliente</h6>
+      <h6>01 800 272 02 91</h6>
+      <h6>Nuestro horario de atención telefónica es 24 hrs.</h6>
+    </div>
+  </footer>
+  <div class="btn-group d-flex justify-content-center" role="group" aria-label="Basic checkbox toggle button group">
+    <input type="checkbox" class="btn-check" id="btncheck1" autocomplete="off">
+    <label class="btn btn-outline-primary" for="btncheck1">Sitio clásico</label>
+
+    <input type="checkbox" class="btn-check" id="btncheck2" autocomplete="off">
+    <label class="btn btn-outline-primary" for="btncheck2">Términos y condiciones</label>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Footer",
+};
+</script>
