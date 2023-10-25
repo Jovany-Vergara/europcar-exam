34252538e83226716a076a6c8cd0f4b2e4d608f3
Jovany Vergara <diego.jobany11@gmail.com>
2023-10-25 09:44:07 -0600

    [main] adjusting footer responsive and change image

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,12 +1,12 @@
 <template>
   <footer>
     <div class="footer-euripcar d-flex justify-content-center">
-      <img src="/src/assets/img/autoclick.jpeg" class="d-inline-block align-text-top m-3" style="width: 5%;">
+      <img src="/src/assets/img/europcar-logo.png" class="d-inline-block align-text-top m-3" style="width: 10%;">
       <div class="line-vertical"></div>
       <div class="m-3">
-        <h6>Atención al cliente</h6>
-        <h6>01 800 272 02 91</h6>
-        <h6>Nuestro horario de atención telefónica es 24 hrs.</h6>
+        <h4>Atención al cliente</h4>
+        <h4>01 800 272 02 91</h4>
+        <h4>Nuestro horario de atención telefónica es 24 hrs.</h4>
       </div>
     </div>
     <div class="btn-group d-flex justify-content-center" role="group" aria-label="Basic checkbox toggle button group">
