2ae5583eaaafae6c02201c761c197c33520444d6
Jovany Vergara <diego.jobany11@gmail.com>
2023-10-24 23:51:30 -0600

    [main] add footer

--- src/App.vue
+++ src/App.vue
@@ -26,12 +26,16 @@
     </div>
   </nav>
   <router-view />
+  <Footer />
 </template>
 
 <script>
+import Footer from './components/Footer.vue';
+
 export default {
   name: "App",
+  components: {
+    Footer,
+  },
 };
 </script>
-
-<style></style>
